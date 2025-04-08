cac20423a2ad04dd2a7d5bb24e0521fbb849b9a5
SamGiKim <rlawns256@naver.com>
2025-04-08 16:16:57 +0900

    , 제거

--- 250331_TRUNK_jhkim/js/stack/copyFileForGraph.php
+++ 250331_TRUNK_jhkim/js/stack/copyFileForGraph.php
@@ -111,7 +111,7 @@ try {
     http_response_code(500);
     echo json_encode([
         'success' => false,
-        'message' => $e->getMessage(),
+        'message' => $e->getMessage()
     ]);
 }
 ?>
