e6b43e4882305ec2885a72265a8c1c6c789befc1
SamGiKim <rlawns256@naver.com>
2025-06-09 17:53:06 +0900

    update diagnosislog
    시간 초까지 나오게 수정

--- 250331_TRUNK_jhkim/js/stack/stack_diagnosis_log.js
+++ 250331_TRUNK_jhkim/js/stack/stack_diagnosis_log.js
@@ -123,5 +123,5 @@ function createPageItem(page, text, isEnabled) {
 
 function formatDate(dateString) {
   const date = new Date(dateString);
-  return `${date.getFullYear()}.${String(date.getMonth() + 1).padStart(2, '0')}.${String(date.getDate()).padStart(2, '0')} ${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`;
+  return `${date.getFullYear()}.${String(date.getMonth() + 1).padStart(2, '0')}.${String(date.getDate()).padStart(2, '0')} ${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}:${String(date.getSeconds()).padStart(2, '0')}`;
 }
